13fde536c672ca7b785603380c9b23c6e8fc7ba1
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-30 22:57:58 +0000

    adds escape function to prevent XXS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,15 +25,15 @@ $(document).ready(function () {
     const tweetHTML = `
     <div class="tweet">
       <header class="tweet-header">
-        <img class="tweet-avatar" src="${tweet.user.avatars}" />
-        <h2 class="tweet-name">${tweet.user.name}</h2>
-        <h2 class="tweet-handler">${tweet.user.handle}</h2>
+        <img class="tweet-avatar" src="${escape(tweet.user.avatars)}" />
+        <h2 class="tweet-name">${escape(tweet.user.name)}</h2>
+        <h2 class="tweet-handler">${escape(tweet.user.handle)}</h2>
       </header>
       <div class="tweet-body">
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
       </div>
       <footer class="tweet-footer">
-        <small class="footer-days">${timeago.format(tweet.created_at)}</small>
+        <small class="footer-days">${escape(timeago.format(tweet.created_at))}</small>
           <span class="icons-footer">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-repeat"></i>
@@ -73,7 +73,7 @@ $(document).ready(function () {
     else {
       console.log('Tweet is submitted!')
       event.preventDefault();
-      const serializedData = $(this).serialize()
+      const serializedData = $(this).serialize();
 
 
       $.post("/tweets", serializedData, () => {
@@ -83,5 +83,11 @@ $(document).ready(function () {
     }
   });
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+
+  };
 
 });
